bdda7071fb0c7b517dcb1ffc0aa7f6f29435980b
jeongho <achiven@gmail.com>
2021-02-06 17:25:58 -0800

    CoverBackgroudSolution init.

---
+++ CoverBackgroundSolution/app/src/main/java/com/example/coverbackgroundsolution/ui/ExecutorFragment.java
@@ -0,0 +1,19 @@
+package com.example.coverbackgroundsolution.ui;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.coverbackgroundsolution.R;
+
+
+public class ExecutorFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_executor, container, false);
+    }
+}
